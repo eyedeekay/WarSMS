b20a1bccd3071f69d16f19ebe378e01cf38537da
cmotc <cmotc@openmailbox.org>
2015-12-14 05:01:41 -0500

    created a debian packager script

---
+++ debian.sh
@@ -0,0 +1,78 @@
+#! /bin/sh
+# Configure your paths and filenames
+SOURCEBINPATH=.
+SOURCEBIN=warsms
+SOURCEDMN=warsms-fake-daemon
+SOURCECONF=warsmsrc
+SOURCEDEF=sms_mms_gateways.json
+SOURCEDOC=README.md
+DEBFOLDER=grepemails
+DEBVERSION=$(date +%Y%m%d)
+
+cd $DEBFOLDER
+
+git pull origin master
+
+DEBFOLDERNAME="../$DEBFOLDER-$DEBVERSION"
+DEBPACKAGENAME=$DEBFOLDER\_$DEBVERSION
+
+rm -rf $DEBFOLDERNAME
+# Create your scripts source dir
+mkdir $DEBFOLDERNAME
+
+# Copy your script to the source dir
+cp -R ./ $DEBFOLDERNAME/
+cd $DEBFOLDERNAME
+
+pwd
+
+# Create the packaging skeleton (debian/*)
+dh_make -s --indep --createorig 
+
+mkdir -p debian/tmp/usr
+cp -R usr debian/tmp/usr
+
+# Remove make calls
+grep -v makefile debian/rules > debian/rules.new 
+mv debian/rules.new debian/rules 
+
+dpkg-source --commit
+
+# debian/install must contain the list of scripts to install 
+# as well as the target directory
+echo usr/bin/$SOURCEBIN usr/bin >> debian/install
+echo usr/bin/$SOURCEDMN usr/bin >> debian/install
+echo etc/$SOURCECONF etc >> debian/install
+echo usr/share/warsms/$SOURCEDEF usr/share/warsms >> debian/install
+cp $SOURCEDOC usr/share/doc/$DEBFOLDER/$SOURCEDOC
+echo usr/share/doc/$DEBFOLDER/$SOURCEDOC usr/share/doc/$DEBFOLDER >> debian/install
+
+echo "Source: $DEBFOLDER
+Section: unknown
+Priority: optional
+Maintainer: cmotc <cmotc@openmailbox.org>
+Build-Depends: debhelper (>= 9)
+Standards-Version: 3.9.5
+Homepage: https://www.github.com/cmotc/grepemails
+#Vcs-Git: git@github.com:cmotc/grepemails
+#Vcs-Browser: https://www.github.com/cmotc/grepemails
+
+Package: $DEBFOLDER
+Architecture: all
+Depends: \${misc:Depends}
+Description: A simple script for extracting emails from a document or string
+" > debian/control
+
+#echo "gsettings set org.gnome.desktop.session session-name awesome-gnome
+#dconf write /org/gnome/settings-daemon/plugins/cursor/active false
+#gconftool-2 --type bool --set /apps/gnome_settings_daemon/plugins/background/active false
+#" > debian/postinst
+# Remove the example files
+rm debian/*.ex
+rm debian/*.EX
+
+dpkg-source --commit
+
+# Build the package.
+# You  will get a lot of warnings and ../somescripts_0.1-1_i386.deb
+debuild -us -uc >> ../log
